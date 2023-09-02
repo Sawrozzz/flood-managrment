edc72b30e633d072094b703ca4ffe48557d83340
[saroj adhikari] <[adhikaribroo281@gmail.com]>
2023-09-02 15:12:21 +0545

    new

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../../components/Header";
 import Mapp from "../../assets/map.jpg";
 import './map.css'
+// import * as Module from './Mapp.jsx'
+
+
 
 const Map = () => {
+  // var map = L.map('map').setView([27.133046, 85.484083], 13);
+
+  // // L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  // // 	maxZoom: 17,
+  // // 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  // // });
+
+  // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //   maxZoom: 19,
+  //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  // }).addTo(map);
+  // var marker = L.marker([51.5, -0.09]).addTo(map);
+  // marker.bindPopup("<div class='custom-popup'>This is a custom popup.</div>");
+
+
+  // let showed = false;
+
+
+  // document.querySelector('.show-area').addEventListener('click', function () {
+  //   if (showed === false) {
+  //     var polygon = L.polygon([
+  //       [27.14, 85.49],
+  //       [27.133, 85.55],
+  //       [27.13305, 85.499],
+
+  //     ]).addTo(map);
+
+  //     showed = true;
+  //   }
+
+  // }
+  // )
   return (
     <div>
       <Header />
-      <div className="map">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6044.969830807474!2d-73.993056!3d40.7513582!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQ1JzEyLjQiTiA3M8KwNTgnNTQuNyJX!5e0!3m2!1svi!2s!4v1600333050767!5m2!1svi!2s"
-        width="1200"
-        height="530"
-        style={{ border: "0" }}
-        allowfullscreen=""
-        loading="lazy"
-      ></iframe>
-      </div>
+      <div className="map"></div>
+      <button className="show-area">show affected region</button>
+
+
+      {/* <Module /> */}
     </div>
   );
 };
---
+++ src/pages/map/Mapp.jsx
@@ -0,0 +1,35 @@
+export default function Mapp (){
+    var map = L.map('map').setView([27.133046,85.484083], 13);
+
+// L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+// 	maxZoom: 17,
+// 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+// });
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([51.5, -0.09]).addTo(map);
+  marker.bindPopup("<div class='custom-popup'>This is a custom popup.</div>");
+
+
+let showed = false;
+
+
+document.querySelector('.show-area').addEventListener('click',function(){
+    if(showed===false){
+        var polygon = L.polygon([
+            [27.14, 85.49 ],
+            [27.133, 85.55],
+            [27.13305, 85.499],
+           
+        ]).addTo(map);
+
+        showed= true;
+    }
+
+}
+)
+
+}
